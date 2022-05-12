40e88e3f1e320f0478da8fc92499f5b98aedd68b
LENOVO <akshaysunny335@gmail.com>
2022-05-12 17:42:11 +0530

    Add project files.

---
+++ Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace dotnetwebapi1.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="dotnetwebapi1.WebApiApplication" Language="C#" %>
